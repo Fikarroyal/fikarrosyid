58868896a798b8b249bfaf6a1e2f38628437bb86
Fikar Royal <121270650+Fikarroyal@users.noreply.github.com>
2025-02-05 23:11:46 +0700

    Add files via upload

---
+++ src/data/resume.tsx
@@ -0,0 +1,245 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, icons, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Fikar Rosyid",
+  initials: "FR",
+  url: "https://fikar.io",
+  location: "Yogyakarta, Indonesia",
+  locationLink: "https://www.google.com/maps/place/yogyakarta",
+  description:
+    "I am a AI Engineer & iOS Developer",
+  summary:
+    "I am currently pursuing my interest in programming. I enrolled in a programming training and learned machine learning, AI engineering, and iOS developer. My favorite part of programming is being able to get solutions and being able to solve problematic programs. I am also familiar with Python and Swift. I always want to learn new era technologies and want to create new technologies in the future. When I am not coding, I enjoy pushing mobile legend rank, actively interacting on social media, and learning new things.",
+  avatarUrl: "/me.jpeg",
+  skills: [
+    "HTML",
+    "CSS",
+    "Typescript",
+    "JavaScript",
+    "Node.js",
+    "Next.js",
+    "React",
+    "PHP",
+    "CodeIgniter",
+    "Laravel",
+    "TailwindCSS",
+    "C++",
+    "MySQL",
+    "PostgreSQL",
+    "SQLite",
+    "MongoDB",
+    "Pyhton",
+    "Swift",
+    "Objective-C",
+    "Kali Linux",
+    "Parrot Linux",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+  ],
+  contact: {
+    email: "fikarrosyid3@gmail.com",
+    tel: "+6282134872268",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/Fikarroyal",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/fikarrosyid/",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      Facebook: {
+        name: "Facebook",
+        url: "https://www.facebook.com/share/161qNe1fuF/?mibextid=wwXIfr",
+        icon: Icons.facebook,
+
+        navbar: true,
+      },
+      Instagram: {
+        name: "Instagram",
+        url: "https://instagram.com/zulfikarsyd",
+        icon: Icons.instagram,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Coming Soon",
+      href: "",
+      badges: [],
+      location: "on-site work",
+      title: "Coming Soon",
+      logoUrl: "/y.png",
+      start: "NO",
+      end: "INFO",
+      description:
+        "COMING SOON",
+    },
+    {
+      company: "Coming Soon",
+      href: "",
+      badges: [],
+      location: "on-site work",
+      title: "Coming Soon",
+      logoUrl: "/y.png",
+      start: "NO",
+      end: "INFO",
+      description:
+        "COMING SOON",
+    },
+  ],
+  education: [
+    {
+      school: "Amikom Yogyakarta University",
+      href: "https://home.amikom.ac.id/",
+      degree: "Bachelor's Degree of Computer Science (BCS)",
+      logoUrl: "/amikom.png",
+      start: "2022",
+      end: "now",
+    },
+  ],
+  projects: [
+    {
+      title: "Royalithic",
+      href: "",
+      dates: "2025 - Now",
+      active: true,
+      description:
+        "Coming Soon",
+      technologies: [
+        "Swift",
+        "Objective-C"
+      ],
+      links: [
+        {
+          type: "Application",
+          href: "",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/r.png",
+    },
+    {
+      title: "Anti Lolot",
+      href: "",
+      dates: "2025 - Now",
+      active: true,
+      description:
+        "Developed an innovative web application called Anti Lolot, designed to accelerate learning and mastery of various subjects with engaging, easy-to-understand materials. Anti Lolot provides interactive content, personalized learning recommendations, and a dynamic experience that makes acquiring new knowledge faster, more effective, and enjoyable.",
+      technologies: [
+        "React",
+        "TypeScript",
+        "JavaScript",
+        "SQLite",
+        "Node.js"
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/antilolot.png",
+    },
+    {
+      title: "Karita",
+      href: "",
+      dates: "2025 - Now",
+      active: true,
+      description:
+        "Developed an e-commerce website application called Karita, which offers a wide range of popular clothing products, recommends products that suit each user's personal style, and creates a unique and enjoyable shopping experience.",
+      technologies: [
+        "Laravel",
+        "PHP",
+        "SQLite",
+        "JavaScript",
+        "Node.js"
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/karita.png",
+    },
+    {
+      title: "Nexa Reach",
+      href: "https://nexareach.domcloud.dev/",
+      dates: "2024 - Now",
+      active: true,
+      description:
+        "Developing an e-commerce website application called Nexa Reach, which offers social media content creation services, as well as developing in-depth & structured business strategies for smooth operations.",
+      technologies: [
+        "Laravel",
+        "PHP",
+        "SQLite",
+        "JavaScript",
+        "Node.js"
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://nexareach.domcloud.dev/",
+          icon: <Icons.globe className="size-3" />,
+        }
+      ],
+      image: "/nexareach.png",
+    },
+  ],
+  hackathons: [
+    {
+      title: "GEMASI 2025 - Empowering Innovation",
+      dates: "January, 2025",
+      location: "1st Place Programming Skill Category",
+      description:
+        "Demonstrated exceptional problem-solving & algorithmic skills in C++ programming, achieving first place in the Programming Skill Category at GEMASI 2025.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-the-north.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+    },
+    {
+      title:
+        "COMING SOON",
+      dates: "2025-2026",
+      location: "Target 1st Place & Gold Medal",
+      description:
+        "COMING SOON",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-western.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [],
+    },
+    {
+      title:
+        "COMING SOON",
+      dates: "2025-2026",
+      location: "Target 1st Place & Gold Medal",
+      description:
+        "COMING SOON",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-western.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [],
+    },
+  ],
+} as const;
